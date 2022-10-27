90a81246d94808c1359d84ed0cd5ff9b5ff7ae7a
ssenan <simonsenan@gmail.com>
2022-10-27 11:47:21 +1100

    config folder structure update

--- src/train.py
+++ src/train.py
@@ -5,36 +5,34 @@ import wandb
 
 from omegaconf import DictConfig, OmegaConf
 from hydra.utils import instantiate
-from pytorch_lightning.loggers import WandbLogger
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 
-@hydra.main(config_path="configs", config_name="template_config")
+@hydra.main(config_path="configs", config_name="train")
 def train(cfg: DictConfig):
     # Keeping track of current config settings in logger
     logger.info(f"Training with config:\n{OmegaConf.to_yaml(cfg)}")
-    run = wandb.init(project="", config=cfg, group="train")
-    pl_logger = WandbLogger()  # include in config?
-    local_logger = instantiate(cfg.logger)
+    run = wandb.init(project=cfg.logger.wandb.project, config=cfg)
+
+    # Placeholder for what loss or metric values we plan to track with wandb
+    wandb.log({"loss": loss})
 
     pl.seed_everything(cfg.seed)
 
     # Check if this works
-    scheduler = instantiate(cfg.scheduler)
-    model = instantiate(cfg.model, variance_schedule=scheduler)
-    train_dl = instantiate(cfg.train)
-    val_dl = instantiate(cfg.val)
+    model = instantiate(cfg.model)
+    train_dl = instantiate(cfg.dataset.train_dl)
+    val_dl = instantiate(cfg.dataset.val_dl)
 
     if cfg.ckpt:
-        ckpt_path = cfg.ckpt
-        model.load_from_checkpoint(ckpt_path)
+        model.load_from_checkpoint(cfg.ckpt)
 
     trainer = pl.Trainer(
         callbacks=cfg.callbacks,
         accelerator=cfg.accelerator,
         devices=cfg.devices,
-        logger=pl_logger
+        logger=cfg.logger.wandb
     )
 
     trainer.fit(model, train_dl, val_dl)
