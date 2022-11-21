6632014193aedd1c7be973c8d8f7f30ed4a6f244
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2022-11-21 20:41:38 +0000

    Add files via upload

---
+++ dna-diffusion/metrics/k_mer_based_metric.py
@@ -0,0 +1,31 @@
+from sourmash import MinHash
+import pandas as pd
+import numpy as np
+
+
+def create_mini_hash_of_a_sequence(seq,minihash): 
+  for k in seq:
+    minihash.add_sequence(k)
+  return minihash
+
+
+def compare_two_sequences_and_return_similarity(seq:str, seq2:str, k:int, n:int):
+  mh1 = MinHash(n=n, ksize=k)
+  mh2 = MinHash(n=n, ksize=k)
+  mh1 = create_mini_hash_of_a_sequence(seq,mh1)
+  mh2 = create_mini_hash_of_a_sequence(seq2,mh2)
+  similarity = round(mh1.similarity(mh2), 5)
+  return similarity
+
+
+def average_similarity(seq:pd.Series, seq2:pd.Series, number_of_hashes:int = 20000, k_sizes:list = [3,7,20]):
+  average_similarities = []
+  sequence_1 = seq.tolist()
+  sequence_2 = seq2.tolist()
+  for k in k_sizes:
+    similarity = compare_two_sequences_and_return_similarity(sequence_1, sequence_2, k, number_of_hashes)
+    average_similarities.append(similarity)
+  average_similarities = np.array(average_similarities)
+  average_similarity = round(average_similarities.mean(), 3)
+  return average_similarity
+
