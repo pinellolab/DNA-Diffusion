e418ac2a7673dade8e8c1130b872d41d88d86980
Aaron Wenteler <aaronwenteler@gmail.com>
2022-12-22 21:30:21 +0100

    clean up imports

--- dna-diffusion/metrics/gene_expression/enformer/main.py
+++ dna-diffusion/metrics/gene_expression/enformer/main.py
@@ -1,13 +1,8 @@
-from dataloader import EnformerDataLoader
 from enformer_inference import EnformerInference
-import pandas as pd
-from Bio import SeqIO
-import os
 
 if __name__ == "__main__":
     data_path = "abc_data/K562.PositivePredictions.txt"
     model = EnformerInference(data_path)
-    # data = EnformerDataLoader(pd.read_csv("abc_data/K562.PositivePredictions.txt", sep="\t"))
     one_hot_seqs = model.data.fetch_sequence()  # this is a dictionary with key being Ensembl ID|Gene Name and the value
                                                 # being the one hot encoded sequence as a torch.Tensor
 
