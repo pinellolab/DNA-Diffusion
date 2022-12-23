ff402fca8692a839b1275d8a3e7bd23624f82bd9
Aaron Wenteler <aaronwenteler@gmail.com>
2022-12-23 17:48:46 +0100

    send model to GPU where data is stored if available

--- dna-diffusion/metrics/gene_expression/enformer/enformer_inference.py
+++ dna-diffusion/metrics/gene_expression/enformer/enformer_inference.py
@@ -1,17 +1,20 @@
 import torch
 import pandas as pd
-from enformer_pytorch import Enformer
+from enformer_lucidrains_pytorch.enformer_pytorch import Enformer
 from dataloader import EnformerDataLoader
 
 
-class EnformerInference:
+class Enformer:
     def __init__(self, data_path: str, model_path="EleutherAI/enformer-official-rough"):
         if torch.cuda.is_available():
+            print("Using NVIDIA GPU")
             device = torch.device("cuda")
         else:
+            print("Using CPU")
             device = torch.device("cpu")
+
         self.device = device
-        self.model = Enformer.from_pretrained(model_path)
+        self.model = Enformer.from_pretrained(model_path).to(device)
         self.data = EnformerDataLoader(pd.read_csv(data_path, sep="\t"))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
