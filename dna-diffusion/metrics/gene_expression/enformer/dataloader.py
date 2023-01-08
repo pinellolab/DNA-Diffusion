4efc68545d09b1713fc5b7eebbd5b03321f1fad3
Aaron Wenteler <aaronwenteler@gmail.com>
2023-01-08 16:57:22 +0000

    Get bedGraph data from DNAse data and convert to BigWig

--- dna-diffusion/metrics/gene_expression/enformer/dataloader.py
+++ dna-diffusion/metrics/gene_expression/enformer/dataloader.py
@@ -9,6 +9,9 @@ import warnings
 from pybedtools import BedTool
 from Bio import SeqIO
 import torch
+import subprocess
+
+import utils
 from enformer_lucidrains_pytorch.enformer_pytorch.data import str_to_one_hot
 
 
@@ -135,7 +138,22 @@ class EnformerDataloaderDNAse:
 
     def make_bed_file(self):
         df = self.data[self.data['chr'] == 'chr1']
-        with open('data/chr1_dnase.bed', 'w') as f:
+        with open('data/chr1_dnase.bedGraph', 'w') as f:
             for _, row in df.iterrows():
                 f.write(f'{row["chr"]}\t{row["start"]}\t{row["end"]}\t{row["total_signal"]}\n')
         f.close()
+        utils.sort_bed_file('data/chr1_dnase.bedGraph')
+        self.bedGraph2bigwig('data/chr1_dnase.bedGraph')
+
+    @staticmethod
+    def bedGraph2bigwig(bedgraph_file):
+        """
+        Converts a bedGraph file to bigWig format. The scripts needed for this project are: mergeBed, bedGraphToBigWig,
+        and fetchChromSizes. They can be found here: http://hgdownload.soe.ucsc.edu/admin/exe/. Don't forget to make
+        them executable.
+        """
+        with open("data/hg38.chrom.sizes", "w") as f:
+            subprocess.run(["./exec/fetchChromSizes", "hg38"], stdout=f)
+        f.close()
+        subprocess.run(["exec/bedGraphToBigWig", bedgraph_file, "data/hg38.chrom.sizes", "data/chr1_dnase.bw"])
+        utils.plot_dnase_track('data/chr1_dnase.bw')
--- dna-diffusion/metrics/gene_expression/enformer/utils.py
+++ dna-diffusion/metrics/gene_expression/enformer/utils.py
@@ -1,6 +1,9 @@
 import time
 import os
 import torch
+import subprocess
+import requests
+from pybedtools import BedTool
 import pandas as pd
 
 
@@ -35,3 +38,50 @@ def create_annotated_dataframe(deepmind_table, output):
     targets = df['target'].str.split(pat="/", n=1, expand=True)
     df['target'] = targets[1]
     return df
+
+
+def sort_bed_file(bed_file):
+    """
+    Sorts a bed file using pybedtools
+    """
+    bedgraph = BedTool(bed_file)
+    sorted_bedgraph = bedgraph.sort()
+    merged_bedgraph = sorted_bedgraph.merge()
+    df_merged = merged_bedgraph.to_dataframe(names=['chrom', 'start', 'end'])
+    df_sorted = sorted_bedgraph.to_dataframe(names=['chrom', 'start', 'end', 'signal'])
+    df_merged['signal'] = df_sorted['signal']
+    merged_bedgraph = BedTool.from_dataframe(df_merged)
+    merged_bedgraph.saveas(bed_file)
+
+
+def plot_dnase_track(path_to_bw):
+    """
+    Plots the DNAse track for chromosome 1
+    """
+    start, end = get_full_chr_coords("1")
+    subprocess.run(["make_tracks_file", "--trackFiles", "data/chr1_dnase.bw", "-o", "data/tracks.ini"],
+                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    subprocess.run(["pyGenomeTracks", "--tracks", "data/tracks.ini", "--region", f"chr1:{start}-{end}",
+                    "--outFileName", "plots/DNAse_chr1.pdf"])
+
+
+def get_full_chr_coords(chrom: str):
+    """
+    Gets the full coordinates for a given chromosome
+    """
+    base_url = "https://rest.ensembl.org/"
+    species = "homo_sapiens"
+
+    response = requests.get(f"{base_url}info/assembly/{species}?content-type=application/json")
+    if response.status_code == 200:
+        data = response.json()
+        chromosomes = data['top_level_region']
+        for chromosome in chromosomes:
+            if chromosome['name'] == chrom:
+                start = 1
+                end = chromosome['length']
+                return start, end
+            else:
+                print(f"Chromosome {chrom} not found")
+    else:
+        print(f"Error: {response.status_code}, {response.reason}")
