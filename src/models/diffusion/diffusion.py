c8dba9b9f7f2bb0d4c20f725f32606f74e6cae9d
Matei Bejan <24592776+mateibejan1@users.noreply.github.com>
2023-01-09 22:43:51 +0200

    Update diffusion.py

--- src/models/diffusion/diffusion.py
+++ src/models/diffusion/diffusion.py
@@ -25,6 +25,7 @@ class DiffusionModel(pl.LightningModule):
         self.save_hyperparameters(ignore=["criterion"])
 
         # create Unet
+        # attempt using hydra.utils.instantiate to instantiate both unet and lr scheduler
         self.model = instantiate_from_config(unet_config)
         self.timesteps = timesteps
 
