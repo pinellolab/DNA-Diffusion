79c57523588cdf34851a5ab6fa591ddab9ccc17b
Younwoo Choi <ywchoi@cs.toronto.edu>
2023-01-25 19:47:52 -0500

    add eval script

--- dna-diffusion/metrics/gene_expression/enformer/eval.py
+++ dna-diffusion/metrics/gene_expression/enformer/eval.py
@@ -1,7 +1,7 @@
+from typing import List
+
 import torch
 import torch.nn.functional as F
-
-from enformer_pytorch import Enformer
 
 def log(t, eps = 1e-20):
     return torch.log(t.clamp(min = eps))
@@ -14,13 +14,27 @@ def pearson_corr_coef(x, y, dim = 1, reduce_dims = (-1,)):
     y_centered = y - y.mean(dim = dim, keepdim = True)
     return F.cosine_similarity(x_centered, y_centered, dim = dim).mean(dim = reduce_dims)
 
-if __name__ == "__main__":
-    data = torch.load('enformer_lucidrains_pytorch/data/test-sample.pt', map_location=torch.device('cpu'))
-    seq, target = data['sequence'], data['target']
-    
-    enformer = Enformer.from_pretrained('EleutherAI/enformer-official-rough')
-    enformer.eval()
+def compute_metrics(model, data, track_idx: List[int]):
+    model.eval()
 
-    enformer_output = enformer(seq, head='human')
-    loss = poisson_loss(enformer_output, target)
-    pcc = pearson_corr_coef(enformer_output, target)
+    seq, target = data['sequence'], data['target']
+    with torch.no_grad():
+        pred = model(seq, head='human')
+
+    if track_idx:
+        pred = pred[:, track_idx]
+        target = target[:, track_idx]
+
+    pl = poisson_loss(pred.T, target.T)
+    pcc = pearson_corr_coef(pred.T, target.T)
+
+    return pl, pcc
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    from enformer_pytorch import Enformer
+
+    data = torch.load('enformer_lucidrains_pytorch/data/test-sample.pt', map_location=torch.device('cpu'))
+    enformer = Enformer.from_pretrained('EleutherAI/enformer-official-rough')
+    import pdb; pdb.set_trace()
+    pl, pcc = compute_metrics(enformer, data, track_idx=[0, 2])
