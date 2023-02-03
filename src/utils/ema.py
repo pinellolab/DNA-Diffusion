fc56858d82c535562d5cc480fde24eb4936e6efd
Jan Sobotka <jsobotka1188@gmail.com>
2023-02-03 01:36:22 +0100

    update ema interface

--- src/utils/ema.py
+++ src/utils/ema.py
@@ -1,12 +1,13 @@
 class EMA:
-    def __init__(self, beta):
+    def __init__(self, model, beta):
         super().__init__()
+        self.ma_model = model
         self.beta = beta
         self.step = 0
 
-    def update_model_average(self, ma_model, current_model):
+    def update_model_average(self, current_model):
         for current_params, ma_params in zip(
-            current_model.parameters(), ma_model.parameters()
+            current_model.parameters(), self.ma_model.parameters()
         ):
             old_weight, up_weight = ma_params.data, current_params.data
             ma_params.data = self.update_average(old_weight, up_weight)
@@ -16,13 +17,13 @@ class EMA:
             return new
         return old * self.beta + (1 - self.beta) * new
 
-    def step_ema(self, ema_model, model, step_start_ema=2000):
+    def step_ema(self, model, step_start_ema=2000):
         if self.step < step_start_ema:
-            self.reset_parameters(ema_model, model)
+            self.reset_parameters(self.model, model)
             self.step += 1
             return
-        self.update_model_average(ema_model, model)
+        self.update_model_average(self.ma_model, model)
         self.step += 1
 
-    def reset_parameters(self, ema_model, model):
-        ema_model.load_state_dict(model.state_dict())
+    def reset_parameters(self, model):
+        self.ma_model.load_state_dict(model.state_dict())
