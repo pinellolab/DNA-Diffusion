d6e84e85fdbd6c2e4fac844da93051579da211dc
cmvcordova <cmvcordova@github.com>
2023-02-03 16:29:45 -0500

    ema parameter fix

--- src/main.py
+++ src/main.py
@@ -6,13 +6,12 @@ import wandb
 from omegaconf import DictConfig, OmegaConf
 from hydra.utils import instantiate
 from hydra.core.config_store import ConfigStore
-from utils.misc import get_parser
-from config import DNADiffusionConfig
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+#from config import DNADiffusionConfig
 
 #_configstore = ConfigStore.instance()
 #_configstore.store(name="dnadiffusion_config", node=DNADiffusionConfig)
 
-#logger = logging.getLogger()
 
 @hydra.main(config_path="configs", config_name="main")
 def main(cfg):
@@ -29,20 +28,21 @@ def main(cfg):
     pl.seed_everything(cfg.seed)
     # Check if this works
     model = instantiate(cfg.model)
-    return
     train_dl = instantiate(cfg.data.train_dl)
-    val_dl = instantiate(cfg.dataset.val_dl)
+    val_dl = instantiate(cfg.data.val_dl)
+    if cfg.ckpt_path:
+        model.load_from_checkpoint(cfg.ckpt_path)
 
-    if cfg.ckpt:
-        model.load_from_checkpoint(cfg.ckpt)
+    model_checkpoint_callback = ModelCheckpoint(dirpath='checkpoints', monitor='val_loss', mode='min', save_top_k=10, save_last=True)
+    lr_monitor_callback = LearningRateMonitor(logging_interval='epoch')
 
     trainer = pl.Trainer(
-        callbacks=cfg.callbacks,
-        accelerator=cfg.accelerator,
-        devices=cfg.devices,
+        callbacks=[model_checkpoint_callback, lr_monitor_callback],
+        accelerator=cfg.trainer.accelerator,
+        devices=cfg.trainer.devices,
         logger=cfg.logger.wandb,
     )
-
+    return
     trainer.fit(model, train_dl, val_dl)
 
 
