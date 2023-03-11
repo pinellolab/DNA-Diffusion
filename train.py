68573779ed880e74d7c21e3f6f1b2f758354edaa
ssenan <simonsenan@gmail.com>
2023-03-11 20:29:28 +0000

    Updating trainer in line with fabric convention

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from dnadiffusion.data.dataloader import LoadingData
 from dnadiffusion.trainer import Trainer
+from dnadiffusion.trainer_fabric import Trainer as TrainerFabric
 
 if __name__ == "__main__":
-    trainer = Trainer()
+    trainer = TrainerFabric()
     trainer.train()
