6a96ebe797db6b691bd4f9fe64ff2799716cabe5
noahweber1 <hamza.zijadic@gmail.com>
2023-04-04 16:54:44 +0200

    formatting test_env

--- test_environment.py
+++ test_environment.py
@@ -12,13 +12,10 @@ def main():
     elif REQUIRED_PYTHON == "python3":
         required_major = 3
     else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+        raise ValueError("Unrecognized python interpreter: {}".format(REQUIRED_PYTHON))
 
     if system_major != required_major:
-        raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
+        raise TypeError("This project requires Python {}. Found: Python {}".format(required_major, sys.version))
     else:
         print(">>> Development environment passes all tests!")
 
