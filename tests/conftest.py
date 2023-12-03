dacdfc69ff23e8a26ff7742afb466bc5cda8da5f
Cameron Smith <cameron.ray.smith@gmail.com>
2023-12-03 17:23:16 -0500

    fix(conftest): catch errors for event loops
    - part of an issue that arises with pytest-asyncio `>=0.23.0`
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,9 @@ import pytest
 
 @pytest.fixture(scope="session")
 def event_loop():
-    loop = asyncio.new_event_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
     yield loop
     loop.close()
