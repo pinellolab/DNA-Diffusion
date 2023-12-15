abee23062049c15e88989bfba3d20385f7c395c1
Cameron Smith <cameron.ray.smith@gmail.com>
2023-12-14 20:54:54 -0500

    fix(flake): reenable bioinformatics group Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- flake.nix
+++ flake.nix
@@ -116,7 +116,7 @@
             dnadiffusion = ./src;
           };
           groups = [
-            # "bioinformatics"
+            "bioinformatics"
             "workflows"
             "lint"
             "test"
