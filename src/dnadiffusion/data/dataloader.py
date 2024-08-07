1bfa72742cca83e20f41e6787ca33de97b9b7e05
ssenan <simonsenan@gmail.com>
2024-08-07 12:46:55 -0400

    fix(data): remove motif related files/functions, to be evaluated externally

--- src/dnadiffusion/data/dataloader.py
+++ src/dnadiffusion/data/dataloader.py
@@ -1,6 +1,4 @@
-import os
 import pickle
-import random
 from typing import Any
 
 import numpy as np
@@ -93,19 +91,6 @@ def load_data(
             number_of_sequences_to_motif_creation=num_sampling_to_compare_cells,
         )
 
-    # Splitting enocde data into train/test/shuffle
-    train_motifs = encode_data["train"]["motifs"]
-    train_motifs_cell_specific = encode_data["train"]["final_subset_motifs"]
-
-    validation_motifs = encode_data["validation"]["motifs"]
-    validation_motifs_cell_specific = encode_data["validation"]["final_subset_motifs"]
-
-    test_motifs = encode_data["test"]["motifs"]
-    test_motifs_cell_specific = encode_data["test"]["final_subset_motifs"]
-
-    shuffle_motifs = encode_data["validation_shuffle"]["motifs"]
-    shuffle_motifs_cell_specific = encode_data["validation_shuffle"]["final_subset_motifs"]
-
     # Creating sequence dataset
     df = encode_data["train"]["df"]
     nucleotides = ["A", "C", "G", "T"]
@@ -130,14 +115,6 @@ def load_data(
 
     # Collecting variables into a dict
     encode_data_dict = {
-        "train_motifs": train_motifs,
-        "train_motifs_cell_specific": train_motifs_cell_specific,
-        "validation_motifs": validation_motifs,
-        "validation_motifs_cell_specific": validation_motifs_cell_specific,
-        "test_motifs": test_motifs,
-        "test_motifs_cell_specific": test_motifs_cell_specific,
-        "shuffle_motifs": shuffle_motifs,
-        "shuffle_motifs_cell_specific": shuffle_motifs_cell_specific,
         "tag_to_numeric": tag_to_numeric,
         "numeric_to_tag": numeric_to_tag,
         "cell_types": cell_types,
@@ -150,119 +127,24 @@ def load_data(
     return encode_data_dict
 
 
-def motifs_from_fasta(fasta: str):
-    print("Computing Motifs....")
-    os.system(f"gimme scan {fasta} -p  JASPAR2020_vertebrates -g hg38 -n 20> train_results_motifs.bed")
-    df_results_seq_guime = pd.read_csv("train_results_motifs.bed", sep="\t", skiprows=5, header=None)
-    df_results_seq_guime["motifs"] = df_results_seq_guime[8].apply(lambda x: x.split('motif_name "')[1].split('"')[0])
+def preprocess_data(input_data_path: str, output_path: str | None = None) -> dict:
+    df = pd.read_csv(input_data_path, sep="\t")
 
-    df_results_seq_guime[0] = df_results_seq_guime[0].apply(lambda x: "_".join(x.split("_")[:-1]))
-    df_results_seq_guime_count_out = df_results_seq_guime[[0, "motifs"]].groupby("motifs").count()
-    return df_results_seq_guime_count_out
+    df_train = df[(df["chr"] != "chr1") & (df["chr"] != "chr2")].reset_index(drop=True)
+    df_validation = df[df["chr"] == "chr2"].reset_index(drop=True)
+    df_test = df[df["chr"] == "chr1"].reset_index(drop=True)
 
-
-def save_fasta(df: pd.DataFrame, name: str, num_sequences: int, seq_to_subset_comp: bool = False) -> str:
-    fasta_path = f"{name}.fasta"
-    save_fasta_file = open(fasta_path, "w")
-    num_to_sample = df.shape[0]
-
-    # Subsetting sequences
-    if num_sequences and seq_to_subset_comp:
-        num_to_sample = num_sequences
-
-    # Sampling sequences
-    print(f"Sampling {num_to_sample} sequences")
-    write_fasta_component = "\n".join(
-        df[["dhs_id", "sequence", "TAG"]]
-        .head(num_to_sample)
-        .apply(lambda x: f">{x[0]}_TAG_{x[2]}\n{x[1]}", axis=1)
-        .values.tolist()
-    )
-    save_fasta_file.write(write_fasta_component)
-    save_fasta_file.close()
-
-    return fasta_path
-
-
-def generate_motifs_and_fastas(
-    df: pd.DataFrame, name: str, num_sequences: int, subset_list: list | None = None
-) -> dict[str, Any]:
-    print("Generating Motifs and Fastas...", name)
-    print("---" * 10)
-
-    # Saving fasta
-    if subset_list:
-        fasta_path = save_fasta(df, f"{name}_{'_'.join([str(c) for c in subset_list])}", num_sequences)
-    else:
-        fasta_path = save_fasta(df, name, num_sequences)
-
-    # Computing motifs
-    motifs = motifs_from_fasta(fasta_path)
-
-    # Generating subset specific motifs
-    final_subset_motifs = {}
-    for comp, v_comp in df.groupby("TAG"):
-        print(comp)
-        c_fasta = save_fasta(v_comp, f"{name}_{comp}", num_sequences, seq_to_subset_comp=True)
-        final_subset_motifs[comp] = motifs_from_fasta(c_fasta)
-
-    return {
-        "fasta_path": fasta_path,
-        "motifs": motifs,
-        "final_subset_motifs": final_subset_motifs,
-        "df": df,
+    encode_data = {
+        "train_df": df_train,
+        "validation_df": df_validation,
+        "test_df": df_test,
     }
 
+    if output_path:
+        with open(output_path, "wb") as f:
+            pickle.dump(encode_data, f)
 
-def preprocess_data(
-    input_csv: str,
-    subset_list: list | None = None,
-    limit_total_sequences: int | None = None,
-    number_of_sequences_to_motif_creation: int = 1000,
-    save_output: bool = True,
-):
-    # Reading the csv file
-    df = pd.read_csv(input_csv, sep="\t")
-
-    # Subsetting the dataframe
-    if subset_list:
-        print(" or ".join([f"TAG == {c}" for c in subset_list]))
-        df = df.query(" or ".join([f'TAG == "{c}" ' for c in subset_list]))
-        print("Subsetting...")
-
-    # Limiting the total number of sequences
-    if limit_total_sequences > 0:
-        print(f"Limiting total sequences to {limit_total_sequences}")
-        df = df.sample(limit_total_sequences)
-
-    # Creating train/test/shuffle groups
-    df_test = df[df["chr"] == "chr1"].reset_index(drop=True)
-    df_train_shuffled = df[df["chr"] == "chr2"].reset_index(drop=True)
-    df_train = df_train = df[(df["chr"] != "chr1") & (df["chr"] != "chr2")].reset_index(drop=True)
-
-    df_train_shuffled["sequence"] = df_train_shuffled["sequence"].apply(
-        lambda x: "".join(random.sample(list(x), len(x)))
-    )
-
-    # Getting motif information from the sequences
-    train = generate_motifs_and_fastas(df_train, "train", number_of_sequences_to_motif_creation, subset_list)
-    test = generate_motifs_and_fastas(df_test, "test", number_of_sequences_to_motif_creation, subset_list)
-    train_shuffled = generate_motifs_and_fastas(
-        df_train_shuffled,
-        "train_shuffled",
-        number_of_sequences_to_motif_creation,
-        subset_list,
-    )
-
-    combined_dict = {"train": train, "test": test, "train_shuffled": train_shuffled}
-
-    # Writing to pickle
-    if save_output:
-        # Saving all train, test, train_shuffled dictionaries to pickle
-        with open("src/dnadiffusion/data/encode_data.pkl", "wb") as f:
-            pickle.dump(combined_dict, f)
-
-    return combined_dict
+    return encode_data
 
 
 class SequenceDataset(Dataset):
@@ -294,3 +176,9 @@ class SequenceDataset(Dataset):
         y = self.c[index]
 
         return x, y
+
+
+if __name__ == "__main__":
+    # preprocess_data("data/HepG2_ENCLB029COU.txt", output_path="data/hepg2_encode_data.pkl")
+    # preprocess_data("data/GM12878_ENCLB441ZZZ.txt", output_path="data/gm12878_encode_data.pkl")
+    preprocess_data("data/K562_ENCLB843GMH.txt", output_path="data/k562_encode_data.pkl")
